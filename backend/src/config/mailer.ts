063b4e7f2b07ee28ccbcaae1a4a1a213c845aa31
mrithika <mrithikashambath@gmail.com>
2026-08-08 18:43:07 +0530

    Added logs

--- backend/src/config/mailer.ts
+++ backend/src/config/mailer.ts
@@ -1,5 +1,8 @@
 import { BrevoClient } from "@getbrevo/brevo";
 
+console.log("BREVO KEY EXISTS:", !!process.env.BREVO_API_KEY);
+console.log("BREVO KEY PREFIX:", process.env.BREVO_API_KEY?.substring(0, 10));
+
 const brevo = new BrevoClient({
   apiKey: process.env.BREVO_API_KEY!,
 });
